52f57023922866bb9b82c8dde3f67f3ac36fa86d
morris-ribs <maumribeiro@gmail.com>
2019-01-09 16:41:29 +0100

    Adding CORS

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import sys
 import requests
 import json
 from flask import Flask, request, Response
+from flask_cors import CORS
 from blockchain import Blockchain
 
 app = Flask(__name__)
- 
+CORS(app)
+
 app_port = sys.argv[1]
 current_node_url = "http://localhost:"+app_port
 b = Blockchain(current_node_url)
